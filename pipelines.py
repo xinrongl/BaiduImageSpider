988d30b24a69df260ac0db8d606bee9374b54f01
xinrongl <34294114+xinrongl@users.noreply.github.com>
2019-11-21 15:52:43 +1100

    Update pipelines.py
    change scrapy.contrib.pipeline tp scrapy.pipelines since it was depreciated in 0.16.0

--- pipelines.py
+++ pipelines.py
@@ -4,7 +4,7 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from scrapy.contrib.pipeline.images import ImagesPipeline
+from scrapy.pipelines.images import ImagesPipeline
 from scrapy.exceptions import DropItem
 from scrapy.http import Request
 
